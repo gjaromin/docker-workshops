e7c2bc995e3d88946b19d0a50059ea3a377fe37b
EC2 Default User <ec2-user@ip-50-0-1-91.us-east-2.compute.internal>
2018-03-13 14:39:12 +0000

    Renamed and updated demos scripts

--- demos/2-node-js/app.js
+++ demos/2-node-js/app.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
     console.log('Reseived RQ from ' + req.connection.remoteAddress);
     
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Hello World');
+    res.end('Hello World from node.js');
     
 }).listen(3000);
 
--- demos/httpd/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -itd -p 8081:80 -v "$PWD"/data:/usr/local/apache2/htdocs/ --name httpd-8081 httpd
