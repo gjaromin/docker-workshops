15198941ff69d4da1252eda3688edab5675da587
EC2 Default User <ec2-user@ip-50-0-1-91.us-east-2.compute.internal>
2018-03-18 14:54:54 +0000

    Repalced httpd with nginx for cluster startup scripts

--- demos/3-httpd-cluster/check-httpd-cluster-status.sh
+++ demos/3-httpd-cluster/check-httpd-cluster-status.sh
@@ -3,7 +3,7 @@
 HOST_ADDRESS="http://ec2-18-221-145-23.us-east-2.compute.amazonaws.com"
 
 
-for i in {001..31}; do
+for i in {001..101}; do
 	url=$HOST_ADDRESS:7$i
         echo "Checking  #: "$url
 	curl -Is $url | head -1
--- demos/3-httpd-cluster/start-httpd-cluster.sh
+++ demos/3-httpd-cluster/start-httpd-cluster.sh
@@ -2,8 +2,9 @@
 
 #todo update port number calculation
 
-for i in {001..30}; do
+for i in {001..100}; do
         echo "Starting container #: "$i
-        docker run -idt -p 7${i}:80 -v /docker-ebs:/usr/local/apache2/htdocs/ --rm --name httpd-cluster-7${i} httpd
+        #docker run -idt -p 7${i}:80 -v "$PWD"/data:/usr/local/apache2/htdocs/ --rm --name httpd-cluster-7${i} httpd
+        docker run -idt -p 7${i}:80 -v "$PWD"/data:/usr/share/nginx/html --rm --name httpd-cluster-7${i} nginx
 	#sleep 5
 done
